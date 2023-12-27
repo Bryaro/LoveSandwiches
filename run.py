4cc5c3be1edd07b3416c95f23fc82a8c0698558a
Bryar Osman <bryarosman.bo@gmail.com>
2023-12-27 14:47:46 +0000

    Collect sales data for the last 5 days in list of column

--- run.py
+++ run.py
@@ -100,7 +100,23 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+def get_last_5_days_sales():
+    """
+    collects data from sales worksheet
+    collects last 5 days sale for each sandwich
+    Returns data as a list of lists
+    """
+    sales = SHEET.worksheet("sales")
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
+
+
 def main():
+
     """
     Run all program functions
     """
@@ -112,4 +128,5 @@ def main():
 
 
 print("welcome to Love Sandwiches Data Automations")
-main()
+# main()
+sales_columns = get_last_5_days_sales()
